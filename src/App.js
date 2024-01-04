07ae1ef0d347ec84e2a01e8b9c98287fa43cf8f3
bathiniharish7 <bathiniharish7@gmail.com>
2024-01-04 16:50:29 +0530

    add weight and Charts are Working Fine

--- src/App.js
+++ src/App.js
@@ -1,50 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
-import BarChart from './components/BarChart';
-import { useState } from 'react';
-import { UserData } from './Data';
-
-import {chartJS} from 'react-chartjs-2'
+import { useEffect, useState } from 'react';
 import LineChart from './components/LineChart2';
+import FormComponent2 from './components/FormComponent2';
+import { Chart as ChartJS } from 'chart.js/auto';
+
 function App() {
-
+  const [data, setData] = useState([]);
   const [userData, setUserData] = useState({
-    labels: UserData.map(data=>data.year),
-    datasets:[
+    labels: [],
+    datasets: [
       {
-        label:"User gained",
-        data:UserData.map(data=>data.userGain),
-        backgroundColor:["blue"],
-        borderWidth:'2',
-        borderColor:'black'
-      }
-    ]
-  })
+        label: 'Weight',
+        data: [],
+        backgroundColor: ['blue'],
+        borderWidth: '2',
+        borderColor: 'black',
+      },
+    ],
+  });
+
+  function addUserData(obj) {
+    setData((prevArray) => [...prevArray, obj]);
+  }
+
+  useEffect(() => {
+    // Update userData whenever data changes
+    setUserData({
+      labels: data.map((obj) => obj.Date),
+      datasets: [
+        {
+          label: 'Weight',
+          data: data.map((obj) => obj.Weight),
+          backgroundColor: ['blue'],
+          borderWidth: '2',
+          borderColor: 'black',
+        },
+      ],
+    });
+  }, [data]);
+
+  
+
   return (
-   <div className='container bg-warning'>
+    <div className='container pt-5 pb-5'>
+      {/* ROW-1 */}
+      <div className='row '>
+       <div className='col-12'>
+       <h1 className='text-center'>Weightloss Tracker</h1>
+       </div>
+      </div>
+      {/* ROW-2 */}
+      <div className='row justify-content-center'>
+        <div className='col-6 '>
+          <FormComponent2 addUserData={addUserData} />
+        </div>
+      </div>
+      {/* ROW-3 */}
+      <div className='row'>
 
-    <div className='row'>
-      <h1 className='text-center'>Weightloss Tracker</h1>
+        <div className='col-12'>
+        <LineChart chartData={userData} />
+        </div>
+
+        
+      </div>
     </div>
-
-    <div className='row justify-content-center'>
-      <button className='btn bg-primary text-light w-50'>Add weight</button>
-    </div>
-
-    <div className='row'>
-
-      <BarChart chartData={userData}/>
-
-    </div>
-
-    <div>
-      <LineChart chartData={userData}/>
-    </div>
-    
-   
-
-
-   </div>
   );
 }
 
