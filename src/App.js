eeb187cb1759fb3c0c2867370d94ea2160c111c8
bathiniharish7 <bathiniharish7@gmail.com>
2024-01-04 12:50:06 +0530

    Bar and line charts implemented and seen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import BarChart from './components/BarChart';
+import { useState } from 'react';
+import { UserData } from './Data';
 
+import {chartJS} from 'react-chartjs-2'
+import LineChart from './components/LineChart2';
 function App() {
+
+  const [userData, setUserData] = useState({
+    labels: UserData.map(data=>data.year),
+    datasets:[
+      {
+        label:"User gained",
+        data:UserData.map(data=>data.userGain),
+        backgroundColor:["blue"],
+        borderWidth:'2',
+        borderColor:'black'
+      }
+    ]
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='container bg-warning'>
+
+    <div className='row'>
+      <h1 className='text-center'>Weightloss Tracker</h1>
     </div>
+
+    <div className='row justify-content-center'>
+      <button className='btn bg-primary text-light w-50'>Add weight</button>
+    </div>
+
+    <div className='row'>
+
+      <BarChart chartData={userData}/>
+
+    </div>
+
+    <div>
+      <LineChart chartData={userData}/>
+    </div>
+    
+   
+
+
+   </div>
   );
 }
 
